e87b58d70633723523391c5e84744d19d8170202
nothyonn <hyonjaeyoung@gmail.com>
2025-06-12 19:50:12 +0900

    정답 파일명 노출 제거, 세션에 정답 저장

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,9 @@ from .routes.captcha import captcha_bp
 
 app = Flask(__name__)
 
+# 세션에 캡차 정답 저장하도록 수정 -> 세션 비밀키 설정
+app.secret_key = "wmt_super_secret_key_123"
+
 # Blueprint 등록 (라우트를 모듈화해서 분리하는 기능)
 # 각각의 라우트를 독립된 모듈로 나누고, main.py와 captcha.py에서 정의된 라우트를 app에 연결함
 app.register_blueprint(main_bp)
